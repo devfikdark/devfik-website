3d6aa6de6d98cffa368ce0dfe8a54742ae49d1d6
zubayerhimel <zhimel007@gmail.com>
2024-10-05 11:58:33 +0600

    feat: add custom font and container

---
+++ src/fonts/index.ts
@@ -0,0 +1,81 @@
+import localFont from "next/font/local";
+
+export const satoshi = localFont({
+  src: [
+    {
+      path: "./Satoshi-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./Satoshi-Italic.woff",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./Satoshi-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./Satoshi-MediumItalic.woff",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./Satoshi-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./Satoshi-BoldItalic.woff",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./Satoshi-Black.woff",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./Satoshi-BlackItalic.woff",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+});
+
+export const gambetta = localFont({
+  src: [
+    {
+      path: "./Gambetta-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./Gambetta-Italic.woff",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./Gambetta-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./Gambetta-MediumItalic.woff",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./Gambetta-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./Gambetta-BoldItalic.woff",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+});
