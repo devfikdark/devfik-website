b9d3ea4c6b99e5db3b30f397a849c263af0047a2
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2024-10-06 21:54:35 +0600

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ['dtj79c1o8b5yh.cloudfront.net'],
+  },
+};
 
 export default nextConfig;
