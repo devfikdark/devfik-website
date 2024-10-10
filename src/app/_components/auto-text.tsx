4760bb684d01b870ae87f4068199abd7be93095f
zubayerhimel <zhimel007@gmail.com>
2024-10-10 22:33:28 +0600

    chore: remove text generate effect

--- src/app/_components/auto-text.tsx
+++ src/app/_components/auto-text.tsx
@@ -1,14 +1,15 @@
-import { TextGenerateEffect } from "@/components/text-generate-effect";
-import React from "react";
+import { BlurFade } from "@/components/blur-fade";
 
 export function AutoText() {
   return (
-    <section className="container max-w-6xl mx-auto mt-40 lg:mt-60">
-      <TextGenerateEffect
-        duration={2}
-        filter={false}
-        words="At Devfik, we believe the devil is in the details. Every solution is custom-built to meet your exact needs, ensuring your business thrives. Your success is our priority, which is why we focus on the small things that make a big difference. Choose Devfik for precision, innovation, and results that last."
-      />
-    </section>
+    <BlurFade delay={0.25} yOffset={10}>
+      <section className="container max-w-6xl mx-auto mt-40 lg:mt-60">
+        <h2 className="text-3xl text-foreground-normal">
+          At Devfik, we believe the devil is in the details. Every solution is custom-built to meet your exact needs,
+          ensuring your business thrives. Your success is our priority, which is why we focus on the small things that
+          make a big difference. Choose Devfik for precision, innovation, and results that last.
+        </h2>
+      </section>
+    </BlurFade>
   );
 }
