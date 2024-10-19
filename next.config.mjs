45351887ed6a620ec74286b71e2caeb4e750244f
Zinnatul Morol <morolswediu@gmail.com>
2024-10-20 01:20:21 +0600

    chore: try to deploy multiple pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
+  async rewrites() {
+    return [
+      { source: "/our-work", destination: "/our-work.html" },
+      { source: "/our-story", destination: "/our-story.html" },
+    ];
+  },
   images: {
     domains: ["dtj79c1o8b5yh.cloudfront.net"],
   },
