303daddc69e8814106ce387856251dccb6db1b8c
Zinnatul Morol <morolswediu@gmail.com>
2024-10-20 15:34:25 +0600

    chore: try to deploy serverless

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      { source: "/our-work", destination: "/our-work.html" },
-      { source: "/our-story", destination: "/our-story.html" },
-    ];
-  },
+  target: 'serverless',
   images: {
     domains: ["dtj79c1o8b5yh.cloudfront.net"],
-  },
+  }
 };
 
 export default nextConfig;
