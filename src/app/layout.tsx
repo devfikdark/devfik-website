6114ddaedc31d862c8edf1b5a2386e2063734f69
zubayerhimel <zhimel007@gmail.com>
2024-10-28 23:47:44 +0600

    chore: add og images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,53 @@ import NextTopLoader from "nextjs-toploader";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { satoshi } from "@/lib/fonts";
+import { siteConfig } from "@/config/site";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "Devfik",
-  description: "Shape The Future",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ["Next.js", "devfik", "team", "debitcredit", "Devfik"],
+  authors: [
+    {
+      name: "shadcn",
+      url: "https://shadcn.com",
+    },
+  ],
+  creator: "shadcn",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "devfik",
+  url: "https://devfik.com",
+  ogImage: "https://dtj79c1o8b5yh.cloudfront.net/devfik-portfolio/og.jpg",
+  description:
+    "We are a team of passionate developers and designers who are dedicated to creating innovative and impactful digital solutions.",
+  links: {
+    twitter: "https://twitter.com/devfik",
+    github: "https://github.com/devfikdark",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
